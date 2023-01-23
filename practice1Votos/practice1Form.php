a5c7ac237c87b2e29f10f2c1c36623f674e92304
DiegoCantarell <diegogccantarell@gmail.com>
2023-01-23 13:38:32 -0600

    Adding form to practice1

---
+++ practice1Votos/practice1Form.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form</title>
+    <h2>Students</h2>
+    <FORM action = "practice1Data.php" method = "POST">
+        Type your name:
+        <input type = "text" name = "name" required>
+        <br><br>
+        
+
+        <input type = "submit" name = "send" value = "Accept">
+    </FORM>
+
+    <?php
+        $fp = fopen("salida.txt","a");
+        if (isset($_POST['enviar'])){
+            $nombre = $_REQUEST['nombre'];
+            $edad = $_REQUEST['edad'];
+            echo "Name: $nombre <br>";
+           
+            fwrite($fp,"$name\n");
+        }
+        fclose($fp);
+    ?>
+
+</head>
+<body>
+    
+</body>
+</html>
