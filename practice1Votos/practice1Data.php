5d4277c1cb60e97bfff1831733d8ce1714e78f92
DiegoCantarell <diegogccantarell@gmail.com>
2023-01-25 00:32:12 -0600

    Updating form labels and buttons

--- practice1Votos/practice1Data.php
+++ practice1Votos/practice1Data.php
@@ -6,10 +6,40 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Data</title>
     <h2>Students</h2>
+    <link rel="stylesheet" href="css/styles.css">
     <form action = "practice1Form.php" method = "POST">
         Type your name:
         <input type = "text" name = "name" required>
         <br><br>
+
+        <div class class="divLine ">Partido 1
+        <img src="images/pri.webp" alt="PRI" width="42" height="42">
+        <input type="radio" name="partido" id="partido1" value="1"/>
+        <label for="partido1">PRI</label>
+        </div>
+        <br><br>
+        
+        <div>Partido 2
+        <img src="images/pan.webp" alt="PAN" width="42" height="42">
+        <input type="radio" name="partido" id="partido2" value="2"/>
+        <label for="partido2">PAN</label>
+        </div>
+        <br><br>
+
+        <div>Partido 3
+        <img src="images/prd.webp" alt="PRD" width="42" height="42">
+        <input type="radio" name="partido" id="partido3" value="3"/>
+        <label for="partido3">PRD</label>
+        </div>
+        <br><br>
+
+        <div>Partido 4
+        <img src="images/equis.webp" alt="NULO" width="42" height="42">
+        <input type="radio" name="partido" id="partido4" value="4"/>
+        <label for="partido4">NULO</label>
+        </div>
+        <br><br>
+
         <input type = "submit" name = "send" value = "Accept">
 </form>
     <?php
@@ -43,10 +73,13 @@
 
         if (isset($_POST['send'])){
             $name = $_REQUEST['name'];
+            $partido = $_REQUEST['partido'];
+            
             
             echo "Name: $name <br>";
+            echo "Partido: $partido <br>";
            
-            fwrite($fp,"$name\n");
+            fwrite($fp,"$name,$partido\n");
         }
         fclose($fp);
     ?>
