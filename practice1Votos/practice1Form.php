74ae71a8da06f3b07e7562a0021c3bc05702fb15
DiegoCantarell <diegogccantarell@gmail.com>
2023-01-24 21:02:46 -0600

    Updating form

--- practice1Votos/practice1Form.php
+++ practice1Votos/practice1Form.php
@@ -6,19 +6,50 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Form</title>
     <h2>Students</h2>
-    <FORM action = "practice1Data.php" method = "POST">
+    <form action = "practice1Data.php" method = "POST">
         Type your name:
         <input type = "text" name = "name" required>
         <br><br>
         
 
         <input type = "submit" name = "send" value = "Accept">
-    </FORM>
+    </form>
 
     <?php
-        $fp = fopen("salida.txt","a");
+
+       /* $filename = 'salida.txt';
+        $somecontent = "Add this to the file\n";
+        if (is_writable($filename)) {
+
+            // In our example we're opening $filename in append mode.
+            // The file pointer is at the bottom of the file hence
+            // that's where $somecontent will go when we fwrite() it.
+            if (!$fp = fopen($filename, 'a')) {
+                echo "Cannot open file ($filename)";
+                exit;
+            }
+
+            // Write $somecontent to our opened file.
+            if (fwrite($fp, $somecontent) === FALSE) {
+                echo "Cannot write to file ($filename)";
+                exit;
+            }
+
+            echo "Success, wrote ($somecontent) to file ($filename)";
+
+            fclose($fp);
+
+        } else {
+            echo "The file $filename is not writable";
+        }*/
+
+
+
+
+
+        $fp = fopen("salida.txt","a+");
         if (isset($_POST['send'])){
-            $nombre = $_REQUEST['name'];
+            $name = $_REQUEST['name'];
             
             echo "Name: $name <br>";
            
