850436f1e20f4ff0ba036b6e91c465bcbc5551d8
DiegoCantarell <diegogccantarell@gmail.com>
2023-01-25 02:37:18 -0600

    Updating view ->  Ver Datos -> adding view mode

--- practice1Votos/practice1VerDatos.php
+++ practice1Votos/practice1VerDatos.php
@@ -10,10 +10,20 @@
     <link rel="stylesheet" href="css/styles.css">
     
     <?php
+        $base = '*';
+        $visual1 = '';
+        $visual2 = '';
+        $visual3 = '';
+        $visual4 = '';
+
         $suma1 = 0;
+        
         $suma2 = 0;
+        
         $suma3 = 0;
+       
         $suma4 = 0;
+       
         $n = 0;
         $uno = '1'; 
 
@@ -42,16 +52,20 @@
 
                 if($partidoData == 1){
                     $suma1 = $suma1 + 1;
+                    $visual1 .= $base;
                 }
 
                 else if($partidoData == 2){
                     $suma2 = $suma2 + 1;
+                    $visual2 .= $base;
                 }
                 else if($partidoData == 3){
                     $suma3 = $suma3 + 1;
+                    $visual3 .= $base;
                 }
                 else if($partidoData == 4){
                     $suma4 = $suma4 + 1;
+                    $visual4 .= $base;
                 }
                 
 
@@ -61,11 +75,18 @@
 
 
         }
-        echo "Partido 1 = $suma1<br>";
-        echo "Partido 2 = $suma2<br>";
-        echo "Partido 3 = $suma3<br>";
-        echo "Partido 4 = $suma4<br>";
+        echo "Partido 1 PRI = $suma1<br>";
+        echo "Partido 2 PAN = $suma2<br>";
+        echo "Partido 3 PRD = $suma3<br>";
+        echo "Partido 4 NULO = $suma4<br>";
         fclose($fp);
+        echo "<br><br><br>";
+        echo "PRI -> $visual1<br>";
+        echo "PAN-> $visual2<br>";
+        echo "PRD -> $visual3<br>";
+        echo "NULO -> $visual4<br>";
+
+
     ?>
 
       
