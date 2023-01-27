5c59f3842358c579a86ef9ae0863361a5ee381d2
DiegoCantarell <diegogccantarell@gmail.com>
2023-01-26 23:44:09 -0600

    Updating form fields

--- practice2Conjuntos/practice2Form.php
+++ practice2Conjuntos/practice2Form.php
@@ -9,8 +9,14 @@
 <body>
     <h1>Operaciones con conjuntos</h1>
     <form action = "practice1Data.php" method = "POST">
-        Number of elements in the set
-        <input type = "text" name = "name" required>
-        <br><br>
+            Number of elements in the set A:
+            <input type = "number" name = "setA" required>
+            <br><br>
+            Number of elements in the set B:
+            <input type = "number" name = "setB" required>
+            <br><br>
+            <input type = "submit" name = "send" value = "Accept">
+            <input type="button" onclick="window.location.href='practice1VerDatos.php';" value="Ver Datos" />
+        </form>
 </body>
 </html>
