35b1ffef96280b72f518bc2ad644eb7bb0089ff5
DiegoCantarell <diegogccantarell@gmail.com>
2023-01-26 23:32:28 -0600

    Adding new practice ->practice2<-  and first steps developing the form

---
+++ practice2Conjuntos/practice2Form.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Operaciones con conjuntos</title>
+</head>
+<body>
+    <h1>Operaciones con conjuntos</h1>
+    <form action = "practice1Data.php" method = "POST">
+        Number of elements in the set
+        <input type = "text" name = "name" required>
+        <br><br>
+</body>
+</html>
