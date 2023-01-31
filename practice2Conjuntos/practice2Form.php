4fcdced0bdb14633cf3bd63be4dcc575ac073d41
DiegoCantarell <diegogccantarell@gmail.com>
2023-01-30 23:04:14 -0600

    Updating form

--- practice2Conjuntos/practice2Form.php
+++ practice2Conjuntos/practice2Form.php
@@ -4,11 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Operaciones con conjuntos</title>
+    <title>Operations with sets</title>
 </head>
 <body>
     <h1>Operaciones con conjuntos</h1>
-    <form action = "practice2Form.php" method = "POST">
+    <form action = "practice2Operations.php" method = "POST">
             Number of elements in the set A:
             <input type = "number" name = "setA" required>
             <br><br>
