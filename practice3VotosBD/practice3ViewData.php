233d5903132f929214bbfa265fbc3fd3c8d5edb7
DiegoCantarell <diegogccantarell@gmail.com>
2023-02-17 00:07:25 -0600

    Updating view the data

--- practice3VotosBD/practice3ViewData.php
+++ practice3VotosBD/practice3ViewData.php
@@ -9,14 +9,35 @@
 <body>
     <h1>View the Data</h1>
     
+    
     <?php
         include "class/sql.php";
         include "class/personas.php";
         include "class/partidos.php";
-        //$partidos = new partidos();
-        //personas = new personas();
+        $partidos = new partidos();
+        $personas = new personas();
 
-        //$personas->getAllData();
+
+        //$id_persona = 8;
+        $personas->getAllData();
+
+        /*echo "Inicio";
+        $link = mysqli_connect("localhost","root","");
+        mysqli_select_db($link,"personas");
+        $result =mysqli_query($link,"SELECT * FROM `personas` ");
+
+        echo "<SELECT name = 'personas'>";
+        while($row = mysqli_fetch_array($result)){
+        $id =  $row['id_persona'];
+        $nombre = $row['nombre'];
+        $partido =  $row['partido'];
+        echo "<option value=$id>$nombre</option>";
+        }
+        echo "</select>";
+        */
+        
     ?>
+    
+    <input type='submit' name = 'enviar' value = 'enviar'>
 </body>
 </html>
