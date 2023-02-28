4fb8d3406ea93c9bd846a2ca2c348cca538cd577
DiegoCantarell <diegogccantarell@gmail.com>
2023-02-27 23:20:11 -0600

    initializing practice 4 - adopt pets

---
+++ practice4Adopt/practice4MainPage.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Adopt</title>
+</head>
+<body>
+    <h1>Adopt a new dog friend!</h1>
+
+    <table>
+    <tr>
+        <th>Catalogue of pets</th>
+        <th>Catalogue of users</th>
+        <th>Add user</th>
+        <th>Add pet</th>
+        <th>Generate pdf</th>
+    </tr>
+    <tr>
+        <td> <input type="button" onclick="window.location.href='practice1VerDatos.php';" value="Catalogue of pets" /></td>
+        <td> <input type="button" onclick="window.location.href='practice1VerDatos.php';" value="Catalogue of users" /></td>
+        <td><input type="button" onclick="window.location.href='practice1VerDatos.php';" value="Add user" /></td>
+        <td> <input type="button" onclick="window.location.href='practice1VerDatos.php';" value="Add pet" /></td>
+        <td><input type="button" onclick="window.location.href='practice1VerDatos.php';" value="Generate pdf" /></td>
+    </tr>
+    </table>
+
+    
+   
+   
+    
+   
+    
+
+
+
+</body>
+</html>
