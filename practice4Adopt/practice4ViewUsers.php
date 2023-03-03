1f0e11ce3046f4531aa98f793f708b280274e9ed
DiegoCantarell <diegogccantarell@gmail.com>
2023-03-02 21:59:28 -0600

    Calling new method ->get all Users info <-

---
+++ practice4Adopt/practice4ViewUsers.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>View users</title>
+</head>
+<body>
+    <h1>View all the users info</h1>
+    <?php
+    include "class/sql.php";
+    include "class/users.php";
+    $users = new users();
+    //$partidos = new partidos();
+    $users->getAllUsers();
+    ?>
+    
+</body>
+</html>
