e1c6068f1af00d855b1ab9cda82c49c8ff136a39
DiegoCantarell <diegogccantarell@gmail.com>
2023-03-05 16:20:53 -0600

    Updating view on backend

---
+++ practice4Adopt/practice4AdoptBackend.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Info about the adoption</h1>
+    <?php
+    $name = isset($_POST["name"])? $_POST["name"]: "";
+    echo"Nombre: ".($name)."<br>";
+    ?>
+</body>
+</html>
