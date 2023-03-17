96b63608960e4c20baff8f6b2d2e5a5467dc80fc
DiegoCantarell <diegogccantarell@gmail.com>
2023-03-16 21:54:43 -0600

    Updating main page

--- practice4Adopt/practice4MainPage.php
+++ practice4Adopt/practice4MainPage.php
@@ -5,37 +5,49 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Adopt</title>
+    <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
+<div class="container">
     <h1>Adopt a new dog friend!</h1>
 
-    <table>
-    <tr>
-        <th>Catalogue of pets</th>
-        <th>Catalogue of users</th>
-        <th>Add pet</th>
-        <th>Add user</th>
-        <th>Adopt a pet</th>
-        <th>Search for adoptions</th>
-        <th>Generate pdf</th>
-    </tr>
-    <tr>
-        <td> <input type="button" onclick="window.location.href='practice4ViewPets.php';" value="Catalogue of pets" /></td>
+    <div class="buttons-container">
+        <button class="catalogue-pets-btn" onclick="window.location.href='practice4ViewPets.php';">
+            <img src="icons/pet-icon.png" alt="Catalogue of pets icon" >
+            Catalogue of pets
+        </button>
 
-        <td> <input type="button" onclick="window.location.href='practice4ViewUsers.php';" value="Catalogue of users" /></td>
+        <button class="catalogue-users-btn" onclick="window.location.href='practice4ViewUsers.php';">
+            <img src="icons/user-icon.png" alt="Catalogue of users icon">
+            Catalogue of users
+        </button>
 
-        <td> <input type="button" onclick="window.location.href='practice4AddPet.php';" value="Add pet" /></td>
+        <button class="add-pet-btn" onclick="window.location.href='practice4AddPet.php';">
+            <img src="icons/add-pet-icon.png" alt="Add pet icon">
+            Add pet
+        </button>
 
-        <td><input type="button" onclick="window.location.href='practice4AddUser.php';" value="Add user" /></td>
+        <button class="add-user-btn" onclick="window.location.href='practice4AddUser.php';">
+            <img src="icons/add-user-icon.png" alt="Add user icon" >
+            Add user
+        </button>
 
-        <td> <input type="button" onclick="window.location.href='practice4Adopt.php';" value="Adopt a pet!" /></td>
+        <button class="adopt-pet-btn" onclick="window.location.href='practice4Adopt.php';">
+            <img src="icons/adopt-pet-icon.png" alt="Adopt a pet icon">
+            Adopt a pet!
+        </button>
 
-        <td> <input type="button" onclick="window.location.href='practice4AdoptionsInfo.php';" value="Search for adoptions" /></td>
+        <button class="search-adoptions-btn" onclick="window.location.href='practice4AdoptionsInfo.php';">
+            <img src="icons/search-icon.png" alt="Search for adoptions icon">
+            Search for adoptions
+        </button>
 
-        <td><input type="button" onclick="window.location.href='htmlPDF.php';" value="Generate pdf" /></td>
-    </tr>
-    </table>
-
+        <button class="generate-pdf-btn" onclick="window.location.href='htmlPDF.php';">
+            <img src="icons/pdf-icon.png" alt="Generate pdf icon">
+            Generate pdf
+        </button>
+    </div>
+</div>
     
    
    
