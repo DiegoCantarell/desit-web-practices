69e56daaccc6580d55131bb7fa0bf6cd134cd6f0
DiegoCantarell <diegogccantarell@gmail.com>
2023-03-16 21:19:41 -0600

    Updating files

--- practice4Adopt/practice4GeneratePDF.php
+++ practice4Adopt/practice4GeneratePDF.php
@@ -1,9 +1,18 @@
 <?php
-echo "Hello PDF";
+/*echo "Hello PDF";
 // Sends output inline to browser
 $mpdf = new \Mpdf\Mpdf();
 $mpdf->WriteHTML('Hello World');
 
-$mpdf->Output();
+$mpdf->Output();*/
+
+require('./fpdf.php');
+
+$pdf=new FPDF();
+$pdf->AddPage();
+$pdf->SetFont('Arial','B',16);
+$pdf->Cell(40,10,'Hello World!');
+$pdf->Output();
+
 
 ?>
