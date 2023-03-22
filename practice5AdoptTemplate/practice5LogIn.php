c1777f901a5c18b4eb36f657b59b0b325b1ab92a
DiegoCantarell <diegogccantarell@gmail.com>
2023-03-22 00:26:34 -0600

    Adding new viw to Log in

---
+++ practice5AdoptTemplate/practice5LogIn.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<title>Adopt a Pet!</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<link rel="stylesheet" href="images/Envision.css" type="text/css" />
+</head>
+<body>
+<div id="wrap">
+  <div id="header">
+    <h1 id="logo-text">Log in</h1>
+    <h2 id="slogan">Adopt a pet can change your life and a pet life...</h2>
+    <div id="header-links">
+      <p> <a href="#">Home</a> | <a href="#">Contact</a> | <a href="#">Site Map</a> </p>
+    </div>
+  </div>
+  <div  id="menu">
+    <ul>
+      <li id="current"><a href="index.php">Home</a></li>
+      <li><a href="#">Catalogue of pets</a></li>
+      
+    </ul>
+  </div>
+  <div id="content-wrap">
+    
+    <div id="main"> <a name="TemplateInfo"></a>
+ 
+      <h3>Ingresar al sistema</h3>
+      <form action="#">
+        <p>
+          <label>User ID </label>
+          <input name="dId" value="ID" type="number" size="30" />
+          <label>Password</label>
+          <input name="dPassword" value="Password" type="text" size="30" />
+          
+        </p>
+      </form>
+      <br />
+    </div>
+  </div>
+  <div id="footer">
+    <p> &copy; 2006 <strong>Your Company</strong> | Design by: <a href="http://www.styleshout.com/">styleshout</a> | Valid <a href="http://validator.w3.org/check?uri=referer">XHTML</a> | <a href="http://jigsaw.w3.org/css-validator/check/referer">CSS</a> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <a href="#">Home</a>&nbsp;|&nbsp; <a href="#">Sitemap</a>&nbsp;|&nbsp; <a href="#">RSS Feed</a> </p>
+  </div>
+</div>
+</body>
+</html>
