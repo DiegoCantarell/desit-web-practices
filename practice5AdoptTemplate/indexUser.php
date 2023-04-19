b33963489939194a675fe34ced747f62783ec01a
DiegoCantarell <diegogccantarell@gmail.com>
2023-04-18 20:49:20 -0600

    Adding controler to view only if a user is loged

--- practice5AdoptTemplate/indexUser.php
+++ practice5AdoptTemplate/indexUser.php
@@ -49,8 +49,8 @@ else{
   </div>
   <div  id="menu">
     <ul>
-    <li ><a href="indexUser.php">Home</a></li>
-      <li id="current"><a href="practice5ViewPetsUser.php">Catalogue of pets</a></li>
+    <li id="current"><a href="indexUser.php">Home</a></li>
+      <li ><a href="practice5ViewPetsUser.php">Catalogue of pets</a></li>
       <li><a href="practice5ViewYourPetsUser.php">View your Pets</a></li>
       <li><a href="practice5ViewAdoptMain.php">Adopt a Pet</a></li>
 
