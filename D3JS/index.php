30d017a896cff7931e5c695feb5d160f076c93a3
DiegoCantarell <diegogccantarell@gmail.com>
2023-05-12 07:31:27 -0600

    Adding D3JS examples

---
+++ D3JS/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+</head>
+<body>
+    <h1><a href="densityPlot.html">Gráfico de densidad</a></h1>
+    <br><br>
+    <h1><a href="histogram.html">Histograma</a></h1>
+    <br><br>
+    <h1><a href="scatterplot.html">Gráfico de dispersión</a></h1>
+    <br><br>
+    <h1><a href="Heatmap.html">Mapa de calor</a></h1>
+    <br><br>
+    <h1><a href="donutChart.html">Gráfico de anillos</a></h1>
+    <br><br>
+    <h1><a href="pieChart.html">Gráfica de pastel</a></h1>
+    <br><br>
+
+</body>
+</html>
