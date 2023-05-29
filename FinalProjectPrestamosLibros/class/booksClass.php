c4c322afa2d2e4dc0cf0a32428f46e0870a0219e
DiegoCantarell <diegogccantarell@gmail.com>
2023-05-29 17:42:59 -0600

    Adding new method to the books class -> get the table with books asociated to the user

--- FinalProjectPrestamosLibros/class/booksClass.php
+++ FinalProjectPrestamosLibros/class/booksClass.php
@@ -74,8 +74,79 @@ class books{
             echo "<table>";
         }
     }
+
+
+    //To get the books not associated with the user
     public function getTableBooksExcludingActualUser($id_owner){
         $sql = "SELECT * FROM `books` where id_owner != '".$id_owner."'";
+        //echo $sql;
+        $this->result= $this->sql->ejecutar($sql);
+        
+        
+        if($this->result-> num_rows >0){
+
+            echo "<table class=\"table table-striped\">"; 
+
+            echo "<tr>";
+            echo "<th></th>";
+            echo "<th>id_book</th>";
+            echo "<th></th>";
+            echo "<th>title</th>";
+            echo "<th></th>";
+            echo "<th>editorial</th>";
+            echo "<th></th>";
+            echo "<th>subject</th>";
+            echo "<th></th>";
+            echo "<th>availability</th>";
+            echo "<th></th>";
+            echo "<th>release_date</th>";
+            echo "<th></th>";
+            echo "<th>owner</th>";
+            echo "<th></th>";
+            echo "<th>id_owner</th>";
+            echo "<th></th>";
+            echo "<th>borrower</th>";
+            echo "<th></th>";
+            echo "<th>borrow_start_date</th>";
+            echo "<th></th>";
+            echo "<th>borrow_end_date</th>";
+            echo "<th></th>";
+            echo "</tr>";
+
+            while($row = $this->result->fetch_assoc()){
+                echo "<tr>";
+                echo "<th></th>";
+                echo"<td>".$row["id_book"]."</td>";
+                echo "<th></th>";
+                echo"<td>".$row["title"]."</td>";
+                echo "<th></th>";
+                echo"<td>".$row["editorial"]."</td>";
+                echo "<th></th>";
+                echo"<td>".$row["subject"]."</td>";
+                echo "<th></th>";
+                echo"<td>".$row["availability"]."</td>";
+                echo "<th></th>";
+                echo"<td>".$row["release_date"]."</td>";
+                echo "<th></th>";
+                echo"<td>".$row["owner"]."</td>";
+                echo "<th></th>";
+                echo"<td>".$row["id_owner"]."</td>";
+                echo "<th></th>";
+                echo"<td>".$row["borrower"]."</td>";
+                echo "<th></th>";
+                echo"<td>".$row["borrow_start_date"]."</td>";
+                echo "<th></th>";
+                echo"<td>".$row["borrow_end_date"]."</td>";
+                echo "<th></th>";
+                echo "</tr>";
+            }
+            echo "<table>";
+        }
+    }
+
+    //To get the books associated with the user
+    public function getTableBooksActualUser($id_owner){
+        $sql = "SELECT * FROM `books` where id_owner = '".$id_owner."'";
         //echo $sql;
         $this->result= $this->sql->ejecutar($sql);
         
