d9c22cb067f5edcdf5488dbe2324c95c182ccc27
DiegoCantarell <diegogccantarell@gmail.com>
2023-05-29 23:21:29 -0600

    Adding methods to users class

--- FinalProjectPrestamosLibros/class/usersClass.php
+++ FinalProjectPrestamosLibros/class/usersClass.php
@@ -94,6 +94,18 @@ class users{
             return $cellphone;
     }
 
+    public function getUserPoints($name){
+        $sql = "SELECT * FROM `users` where name = '".$name."'";
+        //echo $sql;
+        $this->result= $this->sql->ejecutar($sql);
+          if($this->result-> num_rows >0){
+              while($row = $this->result->fetch_assoc()){
+                $points =  $row["points"];
+                echo "<br>The points from the method is:" .$row['points']."<br>";
+              }
+          }
+          return $points;
+    }
     
 
     
@@ -144,6 +156,57 @@ class users{
       return $title;
     }
 
+    //Get a table with all the users except the actual user to rate the users
+    public function getAllUserNamesPoints($id_user){
+        $sql = "SELECT * FROM `users` where id_user != '".$id_user."'";
+        $this->result= $this->sql->ejecutar($sql);
+        //echo $sql;
+        echo "<br><br>";
+        if($this->result-> num_rows >0){
+
+            echo "<table class=\"table table-striped\">"; 
+
+            echo "<tr>";
+            echo "<th></th>";
+            echo "<th>id_user</th>";
+            echo "<th></th>";
+            echo "<th>name</th>";
+            echo "<th></th>";
+            echo "<th>email</th>";
+            echo "<th></th>";
+            echo "<th>registration_date</th>";
+            echo "<th></th>";
+            echo "<th>Points</th>";
+            echo "<th></th>";
+            echo "<th>Rate User</th>";
+            echo "<th></th>";
+            echo "</tr>";
+
+            while($row = $this->result->fetch_assoc()){
+                echo "<form action='../user/rateActionUserBackend.php' method = 'POST'>";
+                echo "<tr>";
+                echo "<th></th>";
+                echo"<td>".$row["id_user"]."</td>";
+                echo "<input type='text' name='id_user' value=".$row["id_user"]." style='display:none;'>";
+                echo "<th></th>";
+                echo"<td>".$row["name"]."</td>";
+                echo "<input type='text' name='name' value=".$row["name"]." style='display:none;'>";
+                echo "<th></th>";
+                echo"<td>".$row["email"]."</td>";
+                echo "<th></th>";
+                echo"<td>".$row["registration_date"]."</td>";
+                echo "<th></th>";
+                echo"<td>".$row["points"]."</td>";
+                echo "<th></th>";
+                echo"<td>  <button   class='btn btn-secondary px-4 px-lg-5' type = 'submit' name = 'send' >Give a Point!</button> </td>";
+                echo "<th></th>";
+                echo "</form>";
+            }
+            echo "<table>";
+        }
+      return $title;
+    }
+
 
     public function updateUser($name,$email,$password,$cellphone){
         //Query para hacer la actualización de contacto
@@ -152,6 +215,15 @@ class users{
         $this->result= $this->sql->ejecutar($sql);
         //echo $sql;
     }
+
+    public function addPoint ($id_user,$points){
+        //Query para hacer la actualización de contacto
+        //$sql = "update contacto set nom = '".$nom."',";
+        $sql = "update users set points='".$points."' where id_user = '".$id_user."'";
+        $this->result= $this->sql->ejecutar($sql);
+        //echo $sql;
+    }
+
 
 
    
