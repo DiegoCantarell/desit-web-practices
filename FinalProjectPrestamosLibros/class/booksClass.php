ea0368aa3a902a3382697d3787ef1ec2c7818647
DiegoCantarell <diegogccantarell@gmail.com>
2023-05-29 21:12:50 -0600

    Adding new method to book class -> deleteBook

--- FinalProjectPrestamosLibros/class/booksClass.php
+++ FinalProjectPrestamosLibros/class/booksClass.php
@@ -76,7 +76,7 @@ class books{
     }
 
 
-    //To get the books not associated with the user
+    //Get the books not associated with the user
     public function getTableBooksExcludingActualUser($id_owner){
         $sql = "SELECT * FROM `books` where id_owner != '".$id_owner."'";
         //echo $sql;
@@ -144,7 +144,7 @@ class books{
         }
     }
 
-    //To get the books associated with the user
+    //Get the books associated with the user
     public function getTableBooksActualUser($id_owner){
         $sql = "SELECT * FROM `books` where id_owner = '".$id_owner."'";
         //echo $sql;
@@ -207,6 +207,76 @@ class books{
                 echo"<td>".$row["borrow_end_date"]."</td>";
                 echo "<th></th>";
                 echo "</tr>";
+            }
+            echo "<table>";
+        }
+    }
+
+
+     //Get the books associated with the user to delete one of them
+    public function getTableBooksActualUserDelete($id_owner){
+        $sql = "SELECT * FROM `books` where id_owner = '".$id_owner."'";
+        //echo $sql;
+        $this->result= $this->sql->ejecutar($sql);
+        
+        
+        if($this->result-> num_rows >0){
+
+            echo "<table class=\"table table-striped\">"; 
+
+            echo "<tr>";
+            echo "<th></th>";
+            echo "<th>id_book</th>";
+            echo "<th></th>";
+            echo "<th>title</th>";
+            echo "<th></th>";
+            echo "<th>editorial</th>";
+            echo "<th></th>";
+            echo "<th>subject</th>";
+            echo "<th></th>";
+            echo "<th>availability</th>";
+            echo "<th></th>";
+            echo "<th>release_date</th>";
+            echo "<th></th>";
+            echo "<th>owner</th>";
+            echo "<th></th>";
+            echo "<th>id_owner</th>";
+            echo "<th></th>";
+            echo "<th>borrower</th>";
+            echo "<th></th>";
+            echo "<th>Delete</th>";
+            echo "<th></th>";
+            echo "</tr>";
+
+            while($row = $this->result->fetch_assoc()){
+                echo "<form action='../user/removeActionUserBackend.php' method = 'POST'>";
+                echo "<tr>";
+                echo "<th></th>";
+                echo"<td>".$row["id_book"]."</td>";
+                echo "<input type='text' name='id_book' value=".$row["id_book"]." style='display:none;'>";
+                echo "<th></th>";
+                echo"<td>".$row["title"]."</td>";
+                echo "<input type='text' name='title' value=".$row["title"]." style='display:none;'>";
+                echo "<th></th>";
+                echo"<td>".$row["editorial"]."</td>";
+                echo "<th></th>";
+                echo"<td>".$row["subject"]."</td>";
+                echo "<th></th>";
+                echo"<td>".$row["availability"]."</td>";
+                echo "<input type='text' name='availability' value=".$row["availability"]." style='display:none;'>";
+                echo "<th></th>";
+                echo"<td>".$row["release_date"]."</td>";
+                echo "<th></th>";
+                echo"<td>".$row["owner"]."</td>";
+                echo "<th></th>";
+                echo"<td>".$row["id_owner"]."</td>";
+                echo "<th></th>";
+                echo"<td>".$row["borrower"]."</td>";
+                echo "<th></th>";
+                echo"<td>  <button   class='btn btn-secondary px-4 px-lg-5' type = 'submit' name = 'send' >Delete</button> </td>";
+                echo "<th></th>";
+                echo "</tr>";
+                echo "</form>";
             }
             echo "<table>";
         }
@@ -289,7 +359,16 @@ class books{
         $this->result= $this->sql->ejecutar($sql);
   
       }
+
+    public function deleteBook($id_book){
+    $sql = "delete from books where id_book='". $id_book."' ";
+    $this->result= $this->sql->ejecutar($sql);
+    //echo $sql;
+    //De esta manera puedo combinar los métodos sin perder la funcionalidad
     
+
+    }
+
 
 
     
