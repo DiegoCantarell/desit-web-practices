311178bd667e0586258c2c0f0c7ab3999968fb2c
DiegoCantarell <diegogccantarell@gmail.com>
2023-05-31 10:59:28 -0600

    Updating db book class with new method -> to delete

--- FinalProjectPrestamosLibros/class/booksClass.php
+++ FinalProjectPrestamosLibros/class/booksClass.php
@@ -442,6 +442,77 @@ class books{
     }
 
 
+     //Get the books associated with the user to delete one of them
+     public function getTableBooksAdminDelete(){
+        $sql = "SELECT * FROM `books` ";
+        //echo $sql;
+        $this->result= $this->sql->ejecutar($sql);
+        
+        
+        if($this->result-> num_rows >0){
+
+            echo "<table class=\"table table-striped\">"; 
+
+            echo "<tr>";
+            echo "<th></th>";
+            echo "<th>id_book</th>";
+            echo "<th></th>";
+            echo "<th>title</th>";
+            echo "<th></th>";
+            echo "<th>editorial</th>";
+            echo "<th></th>";
+            echo "<th>subject</th>";
+            echo "<th></th>";
+            echo "<th>availability</th>";
+            echo "<th></th>";
+            echo "<th>release_date</th>";
+            echo "<th></th>";
+            echo "<th>owner</th>";
+            echo "<th></th>";
+            echo "<th>id_owner</th>";
+            echo "<th></th>";
+            echo "<th>borrower</th>";
+            echo "<th></th>";
+            echo "<th>Delete</th>";
+            echo "<th></th>";
+            echo "</tr>";
+
+            while($row = $this->result->fetch_assoc()){
+                echo "<form action='../admin/removeBookActionAdminBackend.php' method = 'POST'>";
+                
+                echo "<tr>";
+                echo "<th></th>";
+                echo"<td>".$row["id_book"]."</td>";
+                echo "<input type='text' name='id_book' value=".$row["id_book"]." style='display:none;'>";
+                echo "<th></th>";
+                echo"<td>".$row["title"]."</td>";
+                echo "<input type='text' name='title' value=".$row["title"]." style='display:none;'>";
+                echo "<th></th>";
+                echo"<td>".$row["editorial"]."</td>";
+                echo "<th></th>";
+                echo"<td>".$row["subject"]."</td>";
+                echo "<th></th>";
+                echo"<td>".$row["availability"]."</td>";
+                echo "<input type='text' name='availability' value=".$row["availability"]." style='display:none;'>";
+                echo "<th></th>";
+                echo"<td>".$row["release_date"]."</td>";
+                echo "<th></th>";
+                echo"<td>".$row["owner"]."</td>";
+                echo "<th></th>";
+                echo"<td>".$row["id_owner"]."</td>";
+                echo "<th></th>";
+                echo"<td>".$row["borrower"]."</td>";
+                echo "<th></th>";
+                echo"<td>  <button   class='btn btn-secondary px-4 px-lg-5' type = 'submit' name = 'send' >Delete</button> </td>";
+                echo "<th></th>";
+                echo "</tr>";
+                echo "</form>";
+            }
+            echo "<table>";
+        }
+    }
+
+
     public function addBook( $id_book, $title, $editorial,$subject,$availability, $release_date,$owner,$id_owner,$borrower,$borrow_start_date,$borrow_end_date){
         //DEFINIMOS LA EJECUCIÓN Y LA CONSULTA 
   
